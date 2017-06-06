05fc184e4452b1d17f7d04ad00d61bcac36df698
fcbai <piaobomengxiang@163.com>
2017-06-06 19:34:41 +0800

    [N/A] --fcbai add rnn homework

--- example/course06/09_queue_example.py
+++ example/course06/09_queue_example.py
@@ -1,8 +1,3 @@
-""" Example to demonstrate how to use queues
-Author: Chip Huyen
-Prepared for the class CS 20SI: "TensorFlow for Deep Learning Research"
-cs20si.stanford.edu
-"""
 from __future__ import print_function
 import tensorflow as tf
 import numpy as np
@@ -10,11 +5,11 @@ import numpy as np
 N_SAMPLES = 1000
 NUM_THREADS = 4
 # Generating some simple data
-# create 1000 random samples, each is a 1D array from the normal distribution (10, 1)
-data = 10 * np.random.randn(N_SAMPLES, 4) + 1
-# create 1000 random labels of 0 and 1
-target = np.random.randint(0, 2, size=N_SAMPLES)
-
+# todo create 1000 random samples, each is a 1D array from the normal distribution (10, 1)
+data =
+# todo create 1000 random labels of 0 and 1
+target =
+# todo create FIFOQueue
 queue = tf.FIFOQueue(capacity=50, dtypes=[tf.float32, tf.int32], shapes=[[4], []])
 
 enqueue_op = queue.enqueue_many([data, target])
@@ -22,13 +17,13 @@ data_sample, label_sample = queue.dequeue()
 
 # create ops that do something with data_sample and label_sample
 
-# create NUM_THREADS to do enqueue
-qr = tf.train.QueueRunner(queue, [enqueue_op] * NUM_THREADS)
+# todo create NUM_THREADS to do enqueue
+qr =
 with tf.Session() as sess:
     # create a coordinator, launch the queue runner threads.
     coord = tf.train.Coordinator()
     enqueue_threads = qr.create_threads(sess, coord=coord, start=True)
-    for step in xrange(100):  # do to 100 iterations
+    for step in range(100):  # do to 100 iterations
         if coord.should_stop():
             break
         data_batch, label_batch = sess.run([data_sample, label_sample])
